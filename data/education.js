cffa483a33b219b757f6120277de36ed9d20f654
Jesse Copeland <jesse.copeland@gmail.com>
2018-06-15 16:10:58 -1000

    Add education data

---
+++ data/education.js
@@ -0,0 +1,40 @@
+/* Education record template
+  {
+    institution: '',
+    program: '',
+    degree: '',
+    completed: '',
+    city: '',
+    state: '',
+    accomplishments: []
+  }
+ */
+
+ module.exports = [
+  {
+    institution: 'DevLeague',
+    degree: 'JavaScript Web Engineer',
+    program: 'Bootcamp Student',
+    completed: 'January 2015 - April 2015',
+    city: 'Honolulu',
+    state: 'Hawaii',
+    accomplishments: [
+      'Immersive developers bootcamp focusing on JavaScript web stack technologies.',
+      'Worked on 20+ projects used to introduce and utilize various web technologies, including AngularJS, Node.js, Express.js, D3.js, Sass, and MongoDB.',
+      'Participated in two hackathons and various community tech meetups.',
+    ]
+  },
+  {
+    institution: 'Texas State University at San Marcos',
+    degree: 'Bachelor of Business Administration',
+    program: 'Computer Information Systems',
+    completed: 'May 2006',
+    city: 'San Marcos',
+    state: 'Texas',
+    accomplishments: [
+      'GPA: 3.62 (Major: 3.875) Magna Cum Laude',
+      'Dean’s List six semesters',
+      'Beta Gamma Sigma',
+    ]
+  }
+ ];
