88d3e9f1f3c34d9aa7d78b97f11962e10c49062f
Jesse Copeland <jesse.copeland@gmail.com>
2018-06-15 16:10:34 -1000

    Add contact data

---
+++ data/contact.js
@@ -0,0 +1,8 @@
+module.export = {
+  firstName: 'Jesse',
+  lastName: 'Copeland',
+  email: 'jesse.copeland@gmail.com',
+  phone: '18083543054',
+  github: 'mentalgrinds',
+  linkedin: 'jessecopeland'
+};
