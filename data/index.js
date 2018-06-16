9daa128a476fa4017a4696df7963477c4f4124af
Jesse Copeland <jesse.copeland@gmail.com>
2018-06-15 16:11:23 -1000

    Combine data

---
+++ data/index.js
@@ -0,0 +1,9 @@
+const contact = require('./contact');
+const experience = require('./experience');
+const education = require('./education');
+
+module.export = {
+  contact,
+  experience,
+  education,
+}
---
+++ data/skills.js
@@ -0,0 +1,50 @@
+/* 
+{
+  type: '',
+  skills: [
+    '',
+  ]
+}
+*/
+module.export = [
+  {
+    type: 'Modern Web Stack',
+    skills: [
+      'JavaScript/ECMA 2017',
+      'Node.js/Express.js',
+      'HTML5/CSS3',
+      'SPA MV* Apps/React.js/AngularJS',
+      'PostgreSQL/MongoDB',
+    ]
+  },
+  {
+    type: 'Works Well With',
+    skills: [
+      'JavaScript/JQuery',
+      'Docker/Ansible',
+      'Linux/Ubuntu',
+    ]
+  },
+  {
+    type: 'Workflow',
+    skills: [
+      'Docker Compose',
+      'Responsive Web Design',
+      'Mobile Web Development',
+      'Webpack/Gulp',
+      'Standards Compliant',
+      'Cross-browser Compatibility',
+    ]
+  },
+  {
+    type: 'Apps & Tools',
+    skills: [
+      'VS Code/vim/Sublime',
+      'Git/GitHub/Perforce',
+      'Ansible',
+      'Trello/waffle.io/Github project',
+      'JIRA/Bamboo',
+      'Pixelmator/Sketch',
+    ]
+  }
+];
